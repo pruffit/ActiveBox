3ac5d253ef65c23b594877fa575d8786a4a952db
Pruffit <kotlaevdanil@gmail.com>
2021-11-10 18:20:37 +0400

    Create Nav Toggle

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 $(function() {
+    /*FIXED HEADER*/
     let header = $("#header");
     let intro = $("#intro");
     let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
 
+    checkScroll(scrollPos, introH);
+
     $(window).on("scroll load resize", function() {
 
         introH = intro.innerHeight();
-        
+
         scrollPos = $(this).scrollTop();
 
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
         if(scrollPos > introH){
 
             header.addClass("fixed");
@@ -19,6 +26,28 @@ $(function() {
             header.removeClass("fixed");
 
         }
-    });
+    }
+    /*SMOOTH SCROLL*/
+    $("[data-scroll]").on("click", function(event) {
 
+        event.preventDefault();
+
+        let blockId = $(this).data('scroll');
+        let blockOffset = $(blockId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: blockOffset - 80
+        }, 500);
+
+    });
+    /*Nav Toggle*/
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+    navToggle.on("click", function(event) {
+
+        event.preventDefault();
+        nav.toggleClass("show");
+    });
 });
